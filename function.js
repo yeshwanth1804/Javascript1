0d83d2d0031ac7f48b2992dd68641644edd5ee5f
yeshwanth <yeshwanthreddy1805@gmail.com>
2025-09-22 14:15:11 +0530

    changes

--- function.js
+++ function.js
@@ -12,11 +12,24 @@
 
 //add();
 //ES6
-const  add = () => {
-    var a = 67;
-    var b = 68;
-    document.write(a+b);//used to print
-    console.log(a+b);// used for debugging
+// const  add = () => {
+//     var a = 67;
+//     var b = 68;
+//     document.write(a+b);//used to print
+//     console.log(a+b);// used for debugging
 
-};
-add();
+// };
+// add();
+
+// ES7
+//  const add = (a,b) => console.log(a+b);
+//  add (78,90);
+//hoisting - move the declaration of var to the top
+
+// let name = "John";
+// function demo(){
+//     let name = "yeshwanth";
+//     document.write(`Hello, my name is ${name}`);
+// }
+// document.write(`Hello, my name is ${name}`);
+// demo();
