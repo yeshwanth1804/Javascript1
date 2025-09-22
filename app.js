09eed9c424650f054f6b6acfb448109c8d4f4174
yeshwanth <yeshwanthreddy1805@gmail.com>
2025-09-22 11:23:26 +0530

    commit

--- app.js
+++ app.js
@@ -5,4 +5,35 @@ let b = 68;
 const c = 70;
 // const c = 72;
  console.log('value b is ${b}');
+ console.log('value c is ${c}');
 console.log(a);
+
+// data types in java script
+//primitive and non primitive datatypes
+//primitive: number,string,boolean,null,undefined
+//non-primitive: Array,object
+//typeof()-know the type of data
+//this,window- commands to display the functions of js
+
+
+a = 7463;
+console.log(typeof (a));
+name = "john";
+console.log(typeof (name));;
+c = true;
+console.log(typeof (c));
+data = null ;
+console.log(typeof (data));
+d = undefined;
+console.log(typeof (d));
+
+//non primitve
+
+array = [56, "john", true, null]
+console.log(typeof (array));
+data = {
+    name :"john",
+    age: "78",
+    dept: "CSE"
+};
+console.log(typeof (data));
---
+++ function.js
@@ -0,0 +1,22 @@
+// function
+//code reusability
+// function add() {
+//      var a = 67;
+//      var b = 68;
+//      document.write(a+b);//used to print
+//      console.log(a+b);// used for debugging
+
+
+// }
+
+
+//add();
+//ES6
+const  add = () => {
+    var a = 67;
+    var b = 68;
+    document.write(a+b);//used to print
+    console.log(a+b);// used for debugging
+
+};
+add();
